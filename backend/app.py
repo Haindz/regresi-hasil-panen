48c5c756ad005bd6149b0caa35a097c6a035031d
Haindz <hafidzindra54@gmail.com>
2025-10-15 21:46:54 +0700

    Fix: memperbaiki masalah gagal connect antara backend dan frontend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import csv
 from core.exporters.csv_importer import import_from_csv
 from core.operations.regression import LinearRegression
 
@@ -16,6 +17,10 @@ def predict():
     file.save(file_path)
 
     try:
+        with open(file_path, newline='') as f:
+            reader = csv.reader(f)
+            header = next(reader)
+
         matrix = import_from_csv(file_path, skip_header=True)
         model = LinearRegression()
         X, y = model.split_features_target(matrix)
@@ -23,6 +28,7 @@ def predict():
         y_pred = model.predict(X)
 
         return jsonify({
+            "header": header,
             "koefisien": beta.data,
             "prediksi": y_pred.data
         })
